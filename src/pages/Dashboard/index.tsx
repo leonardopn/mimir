38b2dec664f62e89ecd189dfa5d4e5e0c6ddb126
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-04-24 20:17:20 -0300

    feat(Pages: Dashboard): :sparkles: implementa o Header com informações dos livros adicionados recentemente

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-
-import { Container } from "./styles";
+import { FlatList } from "react-native";
+import { CardBook } from "../../components/CardBook";
+import { books } from "../../db/db";
+import { Container, Header, RecentlyAdded, RecentlyAddedHeader, Title } from "./styles";
 
 interface DashboardProps {}
 
 export function Dashboard({}: DashboardProps) {
-    return <Container></Container>;
+    return (
+        <Container>
+            <Header>
+                <RecentlyAdded>
+                    <RecentlyAddedHeader>
+                        <Title>Adicionados recentemente</Title>
+                    </RecentlyAddedHeader>
+                    <FlatList
+                        data={books}
+                        renderItem={({ item }) => (
+                            <CardBook title={item.title} author={item.author} imageUrl={item.image}></CardBook>
+                        )}
+                        keyExtractor={(item) => String(item.key)}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}></FlatList>
+                </RecentlyAdded>
+            </Header>
+        </Container>
+    );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,22 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView`
+    padding: 20px;
+`;
+
+export const Header = styled.View``;
+
+export const RecentlyAdded = styled.View``;
+
+export const RecentlyAddedHeader = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(18)}px;
+    color: ${({ theme }) => theme.colors.text};
+`;
