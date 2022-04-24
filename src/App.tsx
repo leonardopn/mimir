c342e9c87e5b212bbf0817199603e51d3a3d9cad
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-04-24 11:52:32 -0300

    feat: :lipstick: Adicionando theme na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import {
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+    useFonts
+} from "@expo-google-fonts/montserrat";
+import AppLoading from "expo-app-loading";
 import React from "react";
 import { Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./global/styles/theme";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({ Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold });
+
+    if (!fontsLoaded) {
+        return <AppLoading></AppLoading>;
+    }
+
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Olá mundo</Text>
-        </View>
+        <ThemeProvider theme={theme}>
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text>Olá mundo</Text>
+            </View>
+        </ThemeProvider>
     );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+    colors: {
+        primary: "#550044",
+        secondary: "#f4d35e",
+        tertiary: "#32002b",
+        secondary_light: "rgba(255,135,44,0.3)",
+        success: "#12A454",
+        success_light: "rgba(18,164,84,0.5)",
+        attention: "#E83F5B",
+        attention_light: "rgba(232, 63, 91, 0.5)",
+        text: "#545454",
+        text_dark: "#232323",
+        background: "#fff",
+    },
+    fonts: {
+        regular: "Montserrat_400Regular",
+        medium: "Montserrat_500Medium",
+        bold: "Montserrat_700Bold",
+    },
+};
