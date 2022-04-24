4e1f3004a99cb31097f19a3843f09ac8294fd6b9
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-04-24 20:15:17 -0300

    feat: :bento: Adiciona dados estáticos para teste

---
+++ src/db/db.ts
@@ -0,0 +1,32 @@
+export const books = [
+    {
+        key: 1,
+        title: "As vantagens de ser invisível",
+        author: "Stephen Chbosky",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51k0X0RixSL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
+    },
+    {
+        key: 2,
+        title: "Harry Potter e a pedra filosofal",
+        author: "J.K. Rowling",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    },
+    {
+        key: 3,
+        title: "A revolução dos bichos: Um conto de fadas",
+        author: "George Orwell",
+        image: "https://images-na.ssl-images-amazon.com/images/I/61owA5ey3iL._SX324_BO1,204,203,200_.jpg",
+    },
+    {
+        key: 4,
+        title: "Corte de névoa e fúria (Vol. 2 Corte de espinhos e rosas)",
+        author: "Sarah J. Maas",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51NcUns3LZL._SX336_BO1,204,203,200_.jpg",
+    },
+    {
+        key: 5,
+        title: "O Último Desejo - The Witcher: Volume 1",
+        author: "Andrzej Sapkowski",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41zAsIHHgVL._SX342_BO1,204,203,200_.jpg",
+    },
+];
