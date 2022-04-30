f5c5b43bb39d7dc68019016fd9f1f64c04d843e2
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-04-29 23:31:11 -0300

    style(Routes: App): :art: Adiciona formatção prettier e eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable camelcase */
 import {
-    Montserrat_400Regular,
-    Montserrat_500Medium,
-    Montserrat_700Bold,
-    useFonts,
+	Montserrat_400Regular,
+	Montserrat_500Medium,
+	Montserrat_700Bold,
+	useFonts,
 } from "@expo-google-fonts/montserrat";
 import { NavigationContainer } from "@react-navigation/native";
 import AppLoading from "expo-app-loading";
@@ -11,25 +12,28 @@ import { StatusBar, View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { Header } from "./components/Header";
 import theme from "./global/styles/theme";
-import { Dashboard } from "./pages/Dashboard";
 import { AppRoutes } from "./Routes/app.routes";
 
 export default function App() {
-    const [fontsLoaded] = useFonts({ Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold });
+	const [fontsLoaded] = useFonts({
+		Montserrat_400Regular,
+		Montserrat_500Medium,
+		Montserrat_700Bold,
+	});
 
-    if (!fontsLoaded) {
-        return <AppLoading></AppLoading>;
-    }
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
 
-    return (
-        <ThemeProvider theme={theme}>
-            <View style={{ flex: 1 }}>
-                <StatusBar backgroundColor={theme.colors.PRIMARY} />
-                <Header></Header>
-                <NavigationContainer>
-                    <AppRoutes></AppRoutes>
-                </NavigationContainer>
-            </View>
-        </ThemeProvider>
-    );
+	return (
+		<ThemeProvider theme={theme}>
+			<View style={{ flex: 1 }}>
+				<StatusBar backgroundColor={theme.colors.PRIMARY} />
+				<Header />
+				<NavigationContainer>
+					<AppRoutes />
+				</NavigationContainer>
+			</View>
+		</ThemeProvider>
+	);
 }
