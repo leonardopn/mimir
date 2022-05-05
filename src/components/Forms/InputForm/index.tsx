40a1a59edbaa051d0787828cd7711ec590117d6a
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-05-05 19:48:22 -0300

    feat(Component: InputForm): :sparkles: Implementa o componente

---
+++ src/components/Forms/InputForm/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Control, Controller, FieldError, FieldPath, RegisterOptions } from "react-hook-form";
+import { Input } from "../Input";
+import { InputProps } from '../Input/index';
+
+interface InputFormProps<T> extends InputProps {
+	//*React Hook Form
+	error?: FieldError;
+	enableTextError?: boolean;
+	control?: Control<T>;
+	name: FieldPath<T>;
+	rules?: RegisterOptions<T>;
+}
+
+export function InputForm<T>({ name, control, rules, error, ...rest }: InputFormProps<T>) {
+	return (
+		<Controller
+			name={name}
+			control={control}
+			rules={rules}
+			render={({ field: { onChange, value } }) => (
+				<Input
+					onChangeText={onChange}
+					value={value as string}
+					errorMessage={error?.message}
+					{...rest}
+				/>
+			)}
+		/>
+	);
+}
