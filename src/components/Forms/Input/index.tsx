49bd2bdc1a39689e1131b196208b1d38c9a44f7f
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-05-04 23:21:44 -0300

    feat(Components: Input): :sparkles: Estilizando e adicionando label e mensagem de erros no componente

--- src/components/Forms/Input/index.tsx
+++ src/components/Forms/Input/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { TextInputProps } from "react-native";
+import { Container, Error, Label } from "./styles";
 
-import { Container } from "./styles";
+interface InputProps extends TextInputProps {
+	label?: string;
+	error?: string;
+}
 
-interface InputProps extends TextInputProps {}
-
-export function Input({ ...rest }: InputProps) {
-	return <Container {...rest} />;
+export function Input({ label, error, ...rest }: InputProps) {
+	return (
+		<>
+			{error ? <Error>{label}</Error> : label && <Label>{label}</Label>}
+			<Container error={error} {...rest} />
+			{error && <Error>{error}</Error>}
+		</>
+	);
 }
--- src/components/Forms/Input/styles.ts
+++ src/components/Forms/Input/styles.ts
@@ -1,11 +1,28 @@
-import { TextInputProps } from "react-native";
 import styled from "styled-components/native";
-import React from "react";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container: React.FC<TextInputProps> = styled.TextInput`
+interface ContainerProps {
+	error?: string;
+}
+
+export const Container = styled.TextInput<ContainerProps>`
 	flex: 1;
 	background-color: white;
 	border-radius: 10px;
-	height: 40px;
-	padding: 10px;
+	height: ${RFValue(50)}px;
+	padding: 15px;
+	border: ${({ error }) => (error ? "2px" : "1px")} solid
+		${({ theme, error }) => (error ? theme.colors.ATTENTION : theme.colors.TEXT_LIGHT)};
+`;
+
+export const Label = styled.Text`
+	padding-bottom: 4px;
+	font-family: ${({ theme }) => theme.fonts.regular};
+	color: ${({ theme }) => theme.colors.TEXT};
+`;
+
+export const Error = styled.Text`
+	padding: 4px 0;
+	font-family: ${({ theme }) => theme.fonts.medium};
+	color: ${({ theme }) => theme.colors.ATTENTION};
 `;
