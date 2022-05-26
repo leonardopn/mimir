b15404e333ab063aac5850e09eeb8242677cc50c
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-05-26 19:15:44 -0300

    feat: :heavy_plus_sign: Atualiza expo e bibliotecas

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,55 @@
 import {
 	Montserrat_400Regular,
 	Montserrat_500Medium,
-	Montserrat_700Bold,
-	useFonts,
+	Montserrat_700Bold
 } from "@expo-google-fonts/montserrat";
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
-import React from "react";
-import { StatusBar, View } from "react-native";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback, useEffect, useState } from "react";
+import { StatusBar, Text, View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { Header } from "./components/Header";
 import theme from "./global/styles/theme";
 import { AppRoutes } from "./Routes/app.routes";
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		Montserrat_400Regular,
-		Montserrat_500Medium,
-		Montserrat_700Bold,
-	});
+	const [appIsReady, setAppIsReady] = useState(false);
 
-	if (!fontsLoaded) {
-		return <AppLoading />;
+	useEffect(() => {
+		async function prepare() {
+			try {
+				await SplashScreen.preventAutoHideAsync();
+				await Font.loadAsync({
+					Montserrat_400Regular,
+					Montserrat_500Medium,
+					Montserrat_700Bold,
+				});
+
+				await new Promise(resolve => setTimeout(resolve, 5000));
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				setAppIsReady(true);
+			}
+		}
+
+		prepare();
+	}, []);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
+
+	if (!appIsReady) {
+		return <View><Text>Carregando...</Text></View>;
 	}
 
 	return (
 		<ThemeProvider theme={theme}>
-			<View style={{ flex: 1 }}>
+			<View style={{ flex: 1 }} onLayout={onLayoutRootView}>
 				<StatusBar backgroundColor={theme.colors.PRIMARY} />
 				<Header />
 				<NavigationContainer>
