5f492be2f614a7f30ac73c6ba6abe7a4b796971c
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-06-01 22:04:36 -0300

    feat(Components: Input): :sparkles: Implementa alteração de borderColor onFocus

--- src/components/Forms/Input/index.tsx
+++ src/components/Forms/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import { TextInput, TextInputProps } from "react-native";
 import { Container, Error, Label } from "./styles";
 
@@ -8,10 +8,26 @@ export interface InputProps extends TextInputProps {
 }
 
 function Input({ label, errorMessage, ...rest }: InputProps, ref: React.Ref<TextInput>) {
+	const [isFocused, setIsFocused] = useState(false);
+
+	function toggleIsFocused() {
+		setIsFocused(!isFocused);
+	}
+
 	return (
 		<>
 			{errorMessage ? <Error>{label}</Error> : label && <Label>{label}</Label>}
-			<Container error={errorMessage} ref={ref} {...rest} />
+			<Container
+				error={errorMessage}
+				ref={ref}
+				isFocused={isFocused}
+				onFocus={toggleIsFocused}
+				{...rest}
+				onBlur={e => {
+					toggleIsFocused();
+					rest.onBlur && rest.onBlur(e);
+				}}
+			/>
 			{errorMessage && <Error>{errorMessage}</Error>}
 		</>
 	);
--- src/components/Forms/Input/styles.ts
+++ src/components/Forms/Input/styles.ts
@@ -3,6 +3,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 interface ContainerProps {
 	error?: string;
+	isFocused?: boolean;
 }
 
 export const Container = styled(TextInput)<ContainerProps>`
@@ -11,8 +12,13 @@ export const Container = styled(TextInput)<ContainerProps>`
 	border-radius: 10px;
 	height: ${RFValue(50)}px;
 	padding: 15px;
-	border: ${({ error }) => (error ? "2px" : "1px")} solid
-		${({ theme, error }) => (error ? theme.colors.ATTENTION : theme.colors.TEXT_LIGHT)};
+	border: ${({ error, isFocused }) => (error || isFocused ? "2px" : "1px")} solid
+		${({ theme, error, isFocused }) =>
+			error
+				? theme.colors.ATTENTION
+				: isFocused
+				? theme.colors.PRIMARY
+				: theme.colors.TEXT_LIGHT};
 `;
 
 export const Label = styled.Text`
