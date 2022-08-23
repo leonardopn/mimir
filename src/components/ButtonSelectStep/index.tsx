af7102d4d1da436911b06a280507a543aa6a9b23
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-08-22 21:56:22 -0300

    feat(Component: ButtonSelectStep): :sparkles: Adiciona mais tipagem
    Adiciona tipagem de TouchableOpacityProps

--- src/components/ButtonSelectStep/index.tsx
+++ src/components/ButtonSelectStep/index.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Container, Icon, Text, ArrowIcon } from "./styles";
 import { VectorIconProps } from "../VectorIcon";
+import { TouchableOpacityProps } from "react-native";
 
-interface ButtonSelectStepProps {
-	children: string;
+interface ButtonSelectStepProps extends Omit<TouchableOpacityProps, "accessibilityRole"> {
+	children: ReactNode;
 	icon: VectorIconProps;
-    color?: string;
+	color?: string;
 }
 
-export function ButtonSelectStep({ children, icon, color }: ButtonSelectStepProps) {
+export function ButtonSelectStep({ children, icon, color, ...rest }: ButtonSelectStepProps) {
 	return (
-		<Container color={color}>
+		<Container color={color} {...rest}>
 			<Icon {...icon} />
 			<Text>{children}</Text>
 			<ArrowIcon iconFamily="material" name="keyboard-arrow-right" />
