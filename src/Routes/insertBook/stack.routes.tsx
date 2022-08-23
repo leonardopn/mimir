07e3812ce922d9fe83436bfb4949672efddc940a
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-08-22 21:58:04 -0300

    feat(Routes: InsertBookRoutes): :label: Adiciona tipagem para a stack routes

--- src/Routes/insertBook/stack.routes.tsx
+++ src/Routes/insertBook/stack.routes.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StepOne } from "../../pages/InsertBook/StepOne";
+import { StepTwo } from "../../pages/InsertBook/StepTwo";
 
-const Stack = createStackNavigator();
+export type RootStackParamList = {
+	"insertBook-stepOne": undefined;
+	"insertBook-stepTwo": undefined;
+};
 
+const Stack = createStackNavigator<RootStackParamList>();
 export const stackName = "insertBook";
 
 export function InsertBookRoutes() {
 	return (
 		<Stack.Navigator screenOptions={{ headerShown: false }}>
-			<Stack.Screen name={`${stackName}-stepOne`} component={StepOne} />
+			<Stack.Screen name="insertBook-stepOne" component={StepOne} />
+			<Stack.Screen name="insertBook-stepTwo" component={StepTwo} />
 		</Stack.Navigator>
 	);
 }
