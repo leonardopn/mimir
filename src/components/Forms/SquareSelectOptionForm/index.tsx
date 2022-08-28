a876d38667c1766556eab41318117364d1e92c4d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-08-27 23:03:41 -0300

    feat(Component: SquareSelectOptionForm): :sparkles: Cria o componente

---
+++ src/components/Forms/SquareSelectOptionForm/index.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from "react";
+import { Control, Controller, FieldPath, RegisterOptions } from "react-hook-form";
+import { TouchableOpacityProps } from "react-native";
+import { VectorIconProps } from "../../VectorIcon";
+import SquareSelectOption from "../SquareSelectOption";
+
+interface SquareSelectOptionFormProps<T> extends Omit<TouchableOpacityProps, "accessibilityRole"> {
+	children: ReactNode;
+	icon: VectorIconProps;
+	color?: string;
+
+	//*React Hook Form
+	enableTextError?: boolean;
+	control?: Control<T>;
+	name: FieldPath<T>;
+	rules?: RegisterOptions<T>;
+}
+
+export function SquareSelectOptionForm<T>({
+	children,
+	icon,
+	color,
+	name,
+	control,
+	rules,
+	...rest
+}: SquareSelectOptionFormProps<T>) {
+	return (
+		<Controller
+			name={name}
+			control={control}
+			rules={rules}
+			render={({ field: { onChange, value, onBlur, ref } }) => (
+				<SquareSelectOption
+					{...rest}
+					ref={ref}
+					icon={icon}
+					color={color}
+					value={value as boolean}
+					onBlur={onBlur}
+					onPress={() => onChange(!value)}>
+					{children}
+				</SquareSelectOption>
+			)}
+		/>
+	);
+}
---
+++ src/components/Forms/SquareSelectOptionForm/styles.ts
@@ -0,0 +1,27 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import { VectorIcon } from "../../VectorIcon";
+
+export const Container = styled.TouchableOpacity<{ color?: string }>`
+	align-items: center;
+	justify-content: center;
+	padding: 10px;
+	border-radius: 5px;
+	width: 95px;
+	height: 85px;
+	
+	color: ${({ theme }) => theme.colors.WHITE};
+	background-color: ${({ theme, color }) => (color ? color : theme.colors.PRIMARY)};
+`;
+
+export const Icon = styled(VectorIcon)`
+	color: ${({ theme }) => theme.colors.WHITE};
+	font-size: ${RFValue(20)}px;
+`;
+
+export const Text = styled.Text`
+	color: ${({ theme }) => theme.colors.WHITE};
+	font-size: ${RFValue(12)}px;
+	font-family: ${({ theme }) => theme.fonts.medium};
+	font-weight: 600;
+`;
