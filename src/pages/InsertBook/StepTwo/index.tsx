e85465847a9593f38ef75f046c3fdf4539991c63
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-06 20:51:00 -0300

    feat(Component: InsertBook(StepTwo)): :sparkles: Implementa meios de seguir para o próximo step

--- src/pages/InsertBook/StepTwo/index.tsx
+++ src/pages/InsertBook/StepTwo/index.tsx
@@ -1,12 +1,16 @@
+import { StackScreenProps } from "@react-navigation/stack";
 import React, { useEffect } from "react";
 import { Button } from "../../../components/Forms/Button";
 import { SquareSelectOptionForm } from "../../../components/Forms/SquareSelectOptionForm";
 import { HeaderStack } from "../../../components/HeaderStack";
 import { Spacer } from "../../../components/Spacer";
 import { useFormInsertBookContext } from "../../../context/FormInsertBook.context";
+import { RootStackParamList } from "../../../Routes/insertBook/stack.routes";
 import { ButtonWrapper, Container, Content, SubTitle, TextWrapper, Title } from "./styles";
 
-export function StepTwo() {
+interface StepTwoProps extends StackScreenProps<RootStackParamList, "insertBook-stepTwo"> {}
+
+export function StepTwo({ navigation }: StepTwoProps) {
 	const {
 		form: { control, watch, setValue },
 	} = useFormInsertBookContext();
@@ -25,6 +29,10 @@ export function StepTwo() {
 			setValue("isFavorited", false);
 		}
 	}, [isWished]);
+
+	function handleNext(page: keyof RootStackParamList) {
+		navigation.navigate(page);
+	}
 
 	return (
 		<Container>
@@ -52,7 +60,7 @@ export function StepTwo() {
 					</SquareSelectOptionForm>
 				</ButtonWrapper>
 				<Spacer spacing={15} />
-				<Button title="Continuar" />
+				<Button title="Continuar" onPress={() => handleNext("insertBook-stepThree")} />
 			</Content>
 		</Container>
 	);
