9f7c994e6632ab069d773190b81d04f30ea7bc3e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-06 20:50:16 -0300

    feat(Routes: InsertBookRoutes): :sparkles: Adiciona rota para o componente InsertBook(StepThree)

--- src/Routes/insertBook/stack.routes.tsx
+++ src/Routes/insertBook/stack.routes.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StepOne } from "../../pages/InsertBook/StepOne";
 import { StepTwo } from "../../pages/InsertBook/StepTwo";
+import { StepThree } from "../../pages/InsertBook/StepThree";
 
 export type RootStackParamList = {
 	"insertBook-stepOne": undefined;
 	"insertBook-stepTwo": undefined;
+	"insertBook-stepThree": undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -16,6 +18,7 @@ export function InsertBookRoutes() {
 		<Stack.Navigator screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="insertBook-stepOne" component={StepOne} />
 			<Stack.Screen name="insertBook-stepTwo" component={StepTwo} />
+			<Stack.Screen name="insertBook-stepThree" component={StepThree} />
 		</Stack.Navigator>
 	);
 }
