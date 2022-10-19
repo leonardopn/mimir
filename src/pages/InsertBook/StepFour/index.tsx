f199e27a5d90c74a19fb9b27680643200f6cfaeb
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-10-18 21:41:41 -0300

    feat(Component: InsertBook(StepFour)): :sparkles: Implementa completamente o componente
    onSubmit e visual

--- src/pages/InsertBook/StepFour/index.tsx
+++ src/pages/InsertBook/StepFour/index.tsx
@@ -1,29 +1,49 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import React from "react";
 import { Button } from "../../../components/Forms/Button";
 import { HeaderStack } from "../../../components/HeaderStack";
 import { Spacer } from "../../../components/Spacer";
 import { useFormInsertBookContext } from "../../../context/FormInsertBook.context";
 import { RootStackParamList } from "../../../Routes/insertBook/stack.routes";
-import { Container, Content } from "./styles";
+import { Book } from "../../../types/Books";
+import { ImageArea } from "./ImageArea";
+import { Container, Content, SubTitle, Title, TextWrapper } from "./styles";
+import { v4 } from "uuid";
+import { useAppDispatch } from "../../../hooks/useAppDispatch";
+import { addBook } from "../../../store/books.slice";
 
 interface StepFourProps extends StackScreenProps<RootStackParamList, "insertBook-stepFour"> {}
 
 export function StepFour({ navigation }: StepFourProps) {
 	const {
-		form: { control },
+		form: { handleSubmit, reset },
 	} = useFormInsertBookContext();
 
-	function handleNext() {
-		navigation.navigate("insertBook-stepTwo");
-	}
+	const dispatch = useAppDispatch();
+
+	const onSubmit = handleSubmit(data => {
+		const bookToAdd: Book = {
+			id: v4(),
+			...data,
+		};
+		dispatch(addBook(bookToAdd));
+		navigation.navigate("insertBook-stepOne");
+		reset();
+	});
 
 	return (
 		<Container>
 			<HeaderStack title="Cadastro de livro" showGoBack />
-			<Content />
+			<Content>
+				<TextWrapper>
+					<Title>Quer adicionar uma{"\n"}capa para seu livro?</Title>
+					<SubTitle>
+						Se desejar, tire uma foto ou{"\n"}escolha uma de sua galeria.
+					</SubTitle>
+				</TextWrapper>
+				<ImageArea />
+			</Content>
 			<Spacer spacing={10} />
-			<Button title="Continuar" onPress={handleNext} />
+			<Button title="Continuar" onPress={onSubmit} />
 		</Container>
 	);
 }
--- src/pages/InsertBook/StepFour/styles.ts
+++ src/pages/InsertBook/StepFour/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -5,3 +6,20 @@ export const Container = styled.View`
 `;
 
 export const Content = styled.ScrollView``;
+
+export const TextWrapper = styled.View`
+	align-items: center;
+`;
+
+export const Title = styled.Text`
+	font-size: ${RFValue(18)}px;
+	font-family: ${({ theme }) => theme.fonts.bold};
+	color: ${({ theme }) => theme.colors.TEXT};
+	margin-bottom: 10px;
+`;
+
+export const SubTitle = styled.Text`
+	font-size: ${RFValue(15)}px;
+	font-family: ${({ theme }) => theme.fonts.regular};
+	color: ${({ theme }) => theme.colors.TEXT};
+`;
