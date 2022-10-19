65f401f9026c4c2a546f6239247eb76e56d84202
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-10-19 20:03:18 -0300

    fix: :pencil2: Ajusta tipagem do conteúdo da store

--- src/store/store.ts
+++ src/store/store.ts
@@ -13,6 +13,6 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
