b239471e4c39935eecabc7db50fac75e144e83de
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-10-18 21:32:46 -0300

    feat(Redux): :sparkles: Cria o hook useAppDispatch e useAppSelector

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,5 @@
+import type { TypedUseSelectorHook } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
