ea303fdfe24fb3076ef22347e6ec40e4186f64c0
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-08 10:00:47 -0300

    feat(Page: BookView): :sparkles: Adiciona mais informações na view relavantes do livro
    Adiciona autor, editora e descrição

--- src/pages/Book/BookView/index.tsx
+++ src/pages/Book/BookView/index.tsx
@@ -1,9 +1,18 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import { CardBook } from "../../../components/CardBook";
 import { HeaderStack } from "../../../components/HeaderStack";
+import { Spacer } from "../../../components/Spacer";
 import { Title } from "../../../components/Text/Title";
 import { BookStackParamList } from "../../../Routes/Book/stack.routes";
-import { Container, Header } from "./styles";
+import {
+	Container,
+	ContentHeader,
+	Content,
+	BookInformations,
+	MainInfo,
+	SimpleInfo,
+	BookInformationWrapper,
+} from "./styles";
 
 interface BookViewProps extends StackScreenProps<BookStackParamList, "Book-view"> {}
 
@@ -13,10 +22,33 @@ export function BookView({ route }: BookViewProps) {
 	return (
 		<Container>
 			<HeaderStack showGoBack showDivider={false} />
-			<Header>
-				<Title>{book.title}</Title>
-				<CardBook imageUrl={book.image} />
-			</Header>
+			<Content>
+				<ContentHeader>
+					<Title>{book.title}</Title>
+					<CardBook imageUrl={book.image} />
+				</ContentHeader>
+				<BookInformations>
+					<BookInformationWrapper>
+						<MainInfo>
+							Autor: <SimpleInfo>{book.author}</SimpleInfo>
+						</MainInfo>
+					</BookInformationWrapper>
+					<BookInformationWrapper>
+						<MainInfo>
+							Editora: <SimpleInfo>{book.publisher}</SimpleInfo>
+						</MainInfo>
+					</BookInformationWrapper>
+					<Spacer spacing={5} />
+					<BookInformationWrapper>
+						<MainInfo>
+							Descrição:{" "}
+							<SimpleInfo>
+								{book.description ? book.description : "Sem descrição"}
+							</SimpleInfo>
+						</MainInfo>
+					</BookInformationWrapper>
+				</BookInformations>
+			</Content>
 		</Container>
 	);
 }
--- src/pages/Book/BookView/styles.ts
+++ src/pages/Book/BookView/styles.ts
@@ -1,11 +1,50 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
 	flex: 1;
 	padding: 20px;
 `;
 
-export const Header = styled.View`
+export const Content = styled.ScrollView`
+	margin-top: 10px;
+`;
+
+export const ContentHeader = styled.View`
 	justify-content: center;
 	align-items: center;
+	margin-bottom: 5px;
+`;
+
+export const BookInformations = styled.View.attrs(({ theme }) => ({ ...theme.shadow.default }))`
+	background-color: white;
+	border-radius: ${RFValue(10)}px;
+	padding: 15px;
+	margin: 5px;
+`;
+
+export const BookInformationWrapper = styled.View<{ justify?: boolean }>`
+	flex-direction: row;
+	align-items: center;
+	flex-wrap: wrap;
+
+	${({ justify }) =>
+		justify &&
+		css`
+			justify-content: center;
+		`}
+`;
+
+export const MainInfo = styled.Text`
+	font-family: ${({ theme }) => theme.fonts.bold};
+	font-size: ${RFValue(14)}px;
+	color: ${({ theme }) => theme.colors.TEXT};
+	text-align: justify;
+`;
+
+export const SimpleInfo = styled.Text`
+	font-family: ${({ theme }) => theme.fonts.regular};
+	font-size: ${RFValue(13)}px;
+	color: ${({ theme }) => theme.colors.TEXT};
+	text-align: justify;
 `;
