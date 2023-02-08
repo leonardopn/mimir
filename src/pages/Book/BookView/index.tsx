0c1838aff00db30dcc1a0c4f1de8eab78149e4c3
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-07 22:53:05 -0300

    feat(Page: BookView): :sparkles: Cria inicialmente a página

---
+++ src/pages/Book/BookView/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+interface BookViewProps {}
+
+export function BookView({}: BookViewProps) {
+	return (
+		<Container>
+			<Text>Implementar view</Text>
+		</Container>
+	);
+}
