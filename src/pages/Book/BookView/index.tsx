4e521add94fff44c52d97a7537659727b6845c77
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-08 09:25:46 -0300

    feat(Page: BookView): :sparkles: Implementa um header na view
    Com um botão de voltar, capa e o título do livro

--- src/pages/Book/BookView/index.tsx
+++ src/pages/Book/BookView/index.tsx
@@ -1,12 +1,22 @@
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { StackScreenProps } from "@react-navigation/stack";
+import { CardBook } from "../../../components/CardBook";
+import { HeaderStack } from "../../../components/HeaderStack";
+import { Title } from "../../../components/Text/Title";
+import { BookStackParamList } from "../../../Routes/Book/stack.routes";
+import { Container, Header } from "./styles";
 
-interface BookViewProps {}
+interface BookViewProps extends StackScreenProps<BookStackParamList, "Book-view"> {}
 
-export function BookView({}: BookViewProps) {
+export function BookView({ route }: BookViewProps) {
+	const { book } = route.params;
+
 	return (
 		<Container>
-			<Text>Implementar view</Text>
+			<HeaderStack showGoBack showDivider={false} />
+			<Header>
+				<Title>{book.title}</Title>
+				<CardBook imageUrl={book.image} />
+			</Header>
 		</Container>
 	);
 }
--- src/pages/Book/BookView/styles.ts
+++ src/pages/Book/BookView/styles.ts
@@ -2,4 +2,10 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
 	flex: 1;
+	padding: 20px;
+`;
+
+export const Header = styled.View`
+	justify-content: center;
+	align-items: center;
 `;
