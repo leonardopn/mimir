640b82c9fbb3341e2b855f0e7d37e3527e52f846
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-09 10:12:43 -0300

    feat(Page: SearchBook): :construction: Implementa funcionalidade de busca
    Implementa a busca de dados de livros na api do google, porém, necessário muitas melhorias

--- src/pages/Book/SearchBookResult/index.tsx
+++ src/pages/Book/SearchBookResult/index.tsx
@@ -1,14 +1,18 @@
 import { StackScreenProps } from "@react-navigation/stack";
+import { isAxiosError } from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { ActivityIndicator, Text } from "react-native";
+import { CardBookHorizontal } from "../../../components/CardBookHorizontal";
+import { HeaderStack } from "../../../components/HeaderStack";
 import { InsertBookStackParamList } from "../../../Routes/insertBook/stack.routes";
 import { GetGoogleBooksApi, googleBooksApi } from "../../../services/googleBooksApi";
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
 interface SearchBookResultProps
 	extends StackScreenProps<InsertBookStackParamList, "Book-search-result"> {}
 
 export function SearchBookResult({ route }: SearchBookResultProps) {
+	const [apiReturn, setApiReturn] = useState<GetGoogleBooksApi | null>(null);
 	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(false);
 
@@ -17,12 +21,15 @@ export function SearchBookResult({ route }: SearchBookResultProps) {
 	const searchBook = useCallback(async () => {
 		try {
 			const { data } = await googleBooksApi.get<GetGoogleBooksApi>("/", {
-				params: { q: search },
+				params: { q: search, maxResults: 40 },
 			});
 
-			console.log(data.totalItems);
+			setApiReturn(data);
 		} catch (err) {
-			console.log(err);
+			if (isAxiosError(err)) {
+				console.error(err.response?.data);
+			}
+
 			setError(true);
 		} finally {
 			setIsLoading(false);
@@ -41,5 +48,34 @@ export function SearchBookResult({ route }: SearchBookResultProps) {
 			</Container>
 		);
 
-	return <Container />;
+	return (
+		<Container>
+			<HeaderStack showGoBack showDivider={false} title="Cadastro de livro" />
+			<Content>
+				{apiReturn?.items?.map(item => {
+					const { id, volumeInfo } = item;
+					const industryIdentifiers = volumeInfo.industryIdentifiers;
+					const isbn10 =
+						industryIdentifiers?.find(identifier => identifier.type === "ISBN_10")
+							?.identifier || "";
+					const isbn13 =
+						industryIdentifiers?.find(identifier => identifier.type === "ISBN_13")
+							?.identifier || "";
+					const authors = volumeInfo?.authors || [];
+
+					const data = {
+						title: volumeInfo?.title,
+						author: authors[0],
+						publisher: volumeInfo?.publisher,
+						isbn10: isbn10,
+						isbn13: isbn13,
+						description: volumeInfo?.description,
+						image: volumeInfo?.imageLinks?.thumbnail,
+					};
+
+					return <CardBookHorizontal key={id} data={data} />;
+				})}
+			</Content>
+		</Container>
+	);
 }
