2e79e8c71ee7f12244b9b71b82d0c484f74a0d61
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-10 23:55:42 -0300

    feat(Component: Title): :lipstick: Altera o tamanho do texto

--- src/components/Text/Title/styles.ts
+++ src/components/Text/Title/styles.ts
@@ -3,6 +3,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.Text`
 	font-family: ${({ theme }) => theme.fonts.bold};
-	font-size: ${RFValue(18)}px;
+	font-size: ${RFValue(16)}px;
 	color: ${({ theme }) => theme.colors.TEXT};
 `;
