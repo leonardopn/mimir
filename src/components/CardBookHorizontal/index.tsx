17e995a274c47f6d8d1775f707599b4e70c69d96
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-20 21:54:22 -0300

    feat(Component: CardBookHorizontal): :sparkles: Engloba o componente em um TouchableWithoutFeedback

--- src/components/CardBookHorizontal/index.tsx
+++ src/components/CardBookHorizontal/index.tsx
@@ -1,3 +1,4 @@
+import { TouchableWithoutFeedback } from "react-native";
 import { Spacer } from "../Spacer";
 import {
 	CardBookTest,
@@ -18,40 +19,43 @@ interface CardBookHorizontalProps {
 		isbn13?: string;
 		description?: string;
 	};
+	onPress?: () => void;
 }
 
-export function CardBookHorizontal({ data }: CardBookHorizontalProps) {
+export function CardBookHorizontal({ data, onPress }: CardBookHorizontalProps) {
 	const { author, title, publisher, isbn10, isbn13, description, image } = data;
 
 	return (
-		<Container>
-			<CardBookTest imageUrl={image} size={"small"} />
-			<Informations>
-				<InformationWrapper>
-					<MainInfo>{title || "Sem Título"}</MainInfo>
-				</InformationWrapper>
-				<InformationWrapper>
-					<SimpleInfo>{author || "Sem Autor"}</SimpleInfo>
-				</InformationWrapper>
-				<InformationWrapper>
-					<SimpleInfo>{publisher || "Sem editora"}</SimpleInfo>
-				</InformationWrapper>
-				<Spacer spacing={5} />
-				<InformationWrapper>
-					<MainInfo>
-						ISBN-10: <SimpleInfo>{isbn10 || "Sem ISBN-10"}</SimpleInfo>
-					</MainInfo>
-				</InformationWrapper>
-				<InformationWrapper>
-					<MainInfo>
-						ISBN-13: <SimpleInfo>{isbn13 || "Sem ISBN-13"}</SimpleInfo>
-					</MainInfo>
-				</InformationWrapper>
-				<Spacer spacing={5} />
-				<InformationWrapper>
-					<SimpleInfo>{description || "Sem descrição"}</SimpleInfo>
-				</InformationWrapper>
-			</Informations>
-		</Container>
+		<TouchableWithoutFeedback onPress={onPress}>
+			<Container>
+				<CardBookTest imageUrl={image} size={"small"} onPress={onPress} />
+				<Informations>
+					<InformationWrapper>
+						<MainInfo>{title || "Sem Título"}</MainInfo>
+					</InformationWrapper>
+					<InformationWrapper>
+						<SimpleInfo>{author || "Sem Autor"}</SimpleInfo>
+					</InformationWrapper>
+					<InformationWrapper>
+						<SimpleInfo>{publisher || "Sem editora"}</SimpleInfo>
+					</InformationWrapper>
+					<Spacer spacing={5} />
+					<InformationWrapper>
+						<MainInfo>
+							ISBN-10: <SimpleInfo>{isbn10 || "Sem ISBN-10"}</SimpleInfo>
+						</MainInfo>
+					</InformationWrapper>
+					<InformationWrapper>
+						<MainInfo>
+							ISBN-13: <SimpleInfo>{isbn13 || "Sem ISBN-13"}</SimpleInfo>
+						</MainInfo>
+					</InformationWrapper>
+					<Spacer spacing={5} />
+					<InformationWrapper>
+						<SimpleInfo>{description || "Sem descrição"}</SimpleInfo>
+					</InformationWrapper>
+				</Informations>
+			</Container>
+		</TouchableWithoutFeedback>
 	);
 }
