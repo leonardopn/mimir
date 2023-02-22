2812bcc2b85bf97e204c5140a050882d45651b86
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-22 20:59:02 -0300

    feat(Page: InsertBook): :pencil2: Remove tipo não utilizado
    InsertBookContainerDefault tinha uma tipo que não era mais utilizado

--- src/pages/InsertBook/styles.ts
+++ src/pages/InsertBook/styles.ts
@@ -1,13 +1,7 @@
 import { RFValue } from "react-native-responsive-fontsize";
-import styled, { css } from "styled-components/native";
+import styled from "styled-components/native";
 
-export const InsertBookContainerDefault = styled.View<{ isFullScreen: boolean }>`
+export const InsertBookContainerDefault = styled.View`
 	padding: ${RFValue(20)}px;
 	flex: 1;
-
-	${({ isFullScreen }) =>
-		isFullScreen &&
-		css`
-			padding: 0;
-		`}
 `;
