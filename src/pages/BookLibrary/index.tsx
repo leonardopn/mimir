a689d8b1ec1fed8b54c52e508717e87eedac596e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-23 21:38:41 -0300

    feat(Page: BookLibrary): :sparkles: Modifica a página
    Implementa a flat list de livros

--- src/pages/BookLibrary/index.tsx
+++ src/pages/BookLibrary/index.tsx
@@ -1,12 +1,42 @@
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { FlatList } from "react-native";
+import { EmptyLibrary } from "../../components/EmptyLibrary";
+import { HeaderStack } from "../../components/HeaderStack";
+import { useBooks } from "../../hooks/store/useBooks";
+import { isEven } from "../../utils/math";
+import { BookThumb, Container, Content } from "./styles";
 
-interface BookLibraryProps {}
+export function BookLibrary() {
+	const { reverseBooks } = useBooks();
+	const { navigate } = useNavigation();
 
-export function BookLibrary({}: BookLibraryProps) {
 	return (
 		<Container>
-			<Text>Testando</Text>
+			<HeaderStack title="Biblioteca" />
+			<Content>
+				<FlatList
+					data={reverseBooks}
+					renderItem={({ item, index }) => (
+						<BookThumb
+							onPress={() => navigate("Book-view", { book: item })}
+							title={item.title}
+							author={item.author}
+							imageUrl={item.image || ""}
+							size="medium"
+							$isEven={isEven(index)}
+						/>
+					)}
+					numColumns={2}
+					ListEmptyComponent={<EmptyLibrary />}
+					keyExtractor={item => String(item.id)}
+					showsVerticalScrollIndicator={false}
+					contentContainerStyle={{
+						flexGrow: 1,
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				/>
+			</Content>
 		</Container>
 	);
 }
--- src/pages/BookLibrary/styles.ts
+++ src/pages/BookLibrary/styles.ts
@@ -1,6 +1,21 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { CardBook } from "../../components/CardBook";
 
 export const Container = styled.View`
 	padding: ${RFValue(25)}px;
+	flex: 1;
+`;
+
+export const Content = styled.View`
+	flex: 1;
+`;
+
+export const BookThumb = styled(CardBook)<{ $isEven: boolean }>`
+	margin-bottom: ${RFValue(10)}px;
+	${({ $isEven }) =>
+		!$isEven &&
+		css`
+			margin-left: ${RFValue(10)}px;
+		`}
 `;
